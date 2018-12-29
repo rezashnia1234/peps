f76f27cdf5469543d5640002985900b1d05e29a9
MazdakShojaie <hrsh@users.noreply.github.com>
2018-12-30 01:35:13 +0330

    first

---
+++ www/scripts/index.js
@@ -0,0 +1,63 @@
+﻿(function () {
+    "use strict";
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    var inAppBrowserRef;
+    function onDeviceReady() {
+        window.open = cordova.InAppBrowser.open;
+        if (navigator.connection.type) {
+            var networkStatus = navigator.connection.type;
+            if (networkStatus === 'Connection.UNKNOWN' || networkStatus === 'Connection.NONE') {
+                myNotif();
+            } else {
+                loadUrl('http://mos.parseps.com');
+            }
+        }
+    }
+
+    function fakeFunc() {
+        //
+    }
+
+    function myNotif() {
+        navigator.notification.alert('No network available! Please connect to a network and continue again.', fakeFunc, 'Network', 'Ok');
+    }
+
+    function checkNetwork() {
+        if (navigator.connection.type) {
+            var networkStatus = navigator.connection.type;
+            if (networkStatus === 'Connection.UNKNOWN' || networkStatus === 'Connection.NONE') {
+                myNotif();
+            }
+        }
+
+        if (window.localStorage.getItem('uniqueId') === null) {
+            window.localStorage.setItem('uniqueId', device.uuid);
+        }
+    }
+
+    function loadUrl(url) {
+        var target = "_blank";
+        var options = "location=no,hidden=no";
+        inAppBrowserRef = cordova.InAppBrowser.open(url, target, options);
+        inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
+        inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
+        inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
+    }
+
+    function loadStartCallBack() {
+        //
+    }
+
+    function loadStopCallBack() {
+        //
+    }
+
+    function loadErrorCallBack() {
+        //
+    }
+
+    setTimeout(function () {
+        checkNetwork();
+    }, 3000);
+
+})();

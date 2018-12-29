eb082f864a3f203b1086e017ea813cdf6f4ab2ed
MazdakShojaie <hrsh@users.noreply.github.com>
2018-12-30 02:16:07 +0330

    2

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -3,15 +3,22 @@
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
     var inAppBrowserRef;
     function onDeviceReady() {
-        window.open = cordova.InAppBrowser.open;
         if (navigator.connection.type) {
             var networkStatus = navigator.connection.type;
             if (networkStatus === 'Connection.UNKNOWN' || networkStatus === 'Connection.NONE') {
                 myNotif();
             } else {
-                loadUrl('http://mos.parseps.com');
+                setTimeout(function () {
+                    window.open = cordova.InAppBrowser.open;
+                    loadUrl('http://mos.parseps.com');
+                }, 5000);
             }
         }
+    }
+
+    function fuckyou() {
+        navigator.splashscreen.show();
+        navigator.notification.alert('No network available! Please connect to a network and continue again.', fakeFunc, 'Network', 'Ok');
     }
 
     function fakeFunc() {
@@ -59,5 +66,9 @@
     setTimeout(function () {
         checkNetwork();
     }, 3000);
+
+    setTimeout(function () {
+        navigator.splashscreen.hide();
+    }, 5000);
 
 })();
